63689e713c3a2118644d9a1b16bbf6d15bea4e01
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-06 15:38:24 +0200

    MILESTONE 1 DONE

--- js/script.js
+++ js/script.js
@@ -1,2 +1,37 @@
 #! Milestone 
 
+const post = [
+    {
+        id: 1,
+        name: 'Gabriele Chiarello',
+        profilePic: 'https://unsplash.it/300/300?image=12',
+        date: '08/12/2022',
+        text: 'No filter needed',
+        pic: 'https://unsplash.it/300/300?image=40',
+        likes: '243'
+    },
+
+    {
+        id: 2,
+        name: 'Valentino Rossi',
+        profilePic: 'https://unsplash.it/300/300?image=50',
+        date: '06/11/2022',
+        text: 'A pic from my shooting',
+        pic: 'https://unsplash.it/300/300?image=60',
+        likes: '500'
+    },
+
+    {
+        id: 1,
+        name: 'Sandro Tonali',
+        profilePic: 'https://unsplash.it/300/300?image=68',
+        date: '05/22/2022',
+        text: 'In my sparetime I love making photos',
+        pic: 'https://unsplash.it/300/300?image=35',
+        likes: '243'
+    }
+]
+
+console.table(post)
+
+//Milestone 2
